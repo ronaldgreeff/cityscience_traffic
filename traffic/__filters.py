25240c11ff880d36ccb3c895c40607f0e78b3e12
bananarama <12639901+ronaldgreeff@users.noreply.github.com>
2019-04-17 13:38:51 +0100

    Deleted __fitlers.py
    Don't need it

--- traffic/__filters.py
+++
@@ -1,7 +0,0 @@
-from django_filters import rest_framework as filters
-from traffic.models import Record
-
-class RecordFilter(filters.FilterSet):
-	class Meta:
-		model = Record
-		fields = ['road']
